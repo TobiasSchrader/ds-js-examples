49317ab6bfd9a851aa286a592645c7484c5f30af
TobiasSchrader <tschrad1@smail.uni-koeln.de>
2018-11-11 15:49:14 +0100

    Custom console output

--- filereading/converter.js
+++ filereading/converter.js
@@ -9,10 +9,9 @@ function dateiauswahl(evt) {
 	let file = evt.target.files[0];
 	let reader = new FileReader();
 	reader.onload = function() {
-		console.log("read");
+		msg_textarea("file read");
 		let content = XLSX.read(reader.result, {type: 'binary'});
 		//document.getElementById("output").innerHTML = 
-		console.log(JSON.stringify(content));
 		display(content);
 	}
 	reader.readAsBinaryString(file);
@@ -27,26 +26,24 @@ function dateiauswahl(evt) {
 		Object.keys(table).forEach( function(entry) {
 			let entryLine=getLine(entry);
 			if (entryLine > currentLine) {
-				console.log("created new line");
+				msg_textarea("created new line");
 				line=document.createElement("tr");
-				console.log(typeof(line));
 				currentLine=entryLine;
 				tableElement.appendChild(line);
 			}
 			if (entryLine) {
-				console.log("added cell");
+				msg_textarea("added cell");
 				cell = document.createElement("td");
 				cell.innerHTML = table[entry].w;
-				console.log(typeof(line));
 				line.appendChild(cell);
 			}
 		});
 	}
 	
 	function getLine(key) {
-		console.log(key);
+		msg_textarea(key);
 		line=Number(key.replace(/\D/g, ""));
-		console.log(line);
+		msg_textarea(line);
 		return line;
 		
 	}
