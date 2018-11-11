b53f387b893785634e2d5b8e223ff8cd67bd1e15
martin-schulte <abospam@hbdb.de>
2018-11-11 11:33:08 +0100

    moved msg2 to msg_textarea in lib/msg_textarea.js

--- dom/dom.js
+++ dom/dom.js
@@ -1,16 +1,3 @@
-function msg2(msg) {
-    let destination = document.getElementById( "msg_textarea" );
-    if ( !destination ) {
-        destination = document.createElement( "textarea" );
-        destination.id = "msg_textarea";
-        destination.cols = 120;
-        destination.rows = 20;
-        document.body.appendChild( destination );
-    }
-    destination.value = destination.value + msg + "\n";
-    destination.setSelectionRange( destination.textLength, destination.textLength );
-}
-
 function textify_structure( element, indent ) {
     let retval = '';
     let nodeType = element.nodeType;
