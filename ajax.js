c2d58a7d24aee06e95bce92e1dd2505c42ed92d2
martin-schulte <abospam@hbdb.de>
2018-11-24 14:30:38 +0100

    Added ajax.{html,js}

---
+++ ajax.js
@@ -0,0 +1,14 @@
+msg_textarea( 'ajax.js' );
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+  let response = this.responseText.replace( /\n+$/, '' );
+  msg_textarea( this.readyState+" "+this.status+" "+response );
+//  if ( this.readyState == 4 && this.status == 200 ) {
+//    msg_textarea( this.responseText );
+//  }
+};
+
+xhttp.open( "GET", "http://schulte.lxht.de/cgi/person.sh?99", true );
+xhttp.send();
